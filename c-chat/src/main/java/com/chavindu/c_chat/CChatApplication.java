bb05b583b772b61338c424d17a0ba8e0e09aeb93
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2025-01-17 05:10:10 +0530

    first commit

---
+++ c-chat/src/main/java/com/chavindu/c_chat/CChatApplication.java
@@ -0,0 +1,13 @@
+package com.chavindu.c_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CChatApplication.class, args);
+	}
+
+}
---
+++ c-chat/src/test/java/com/chavindu/c_chat/CChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chavindu.c_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
