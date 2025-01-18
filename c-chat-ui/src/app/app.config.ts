04373e6dc2589b5527ceda056dcb21cc390d4b37
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2025-01-19 02:01:45 +0530

    configured keycloak

--- c-chat-ui/src/app/app.config.ts
+++ c-chat-ui/src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, inject, provideAppInitializer} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {KeycloakService} from "./utils/keycloak/keycloak.service";
+import {keycloakHttpInterceptor} from "./utils/http/keycloak-http.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
+    provideAppInitializer(() => {
+      const initFn = ((key: KeycloakService) => {
+        return () => key.init();
+      })(inject(KeycloakService));
+      return initFn();
+    })
+  ]
 };
